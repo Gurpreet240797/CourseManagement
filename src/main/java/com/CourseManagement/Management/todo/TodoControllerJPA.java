293e7bf78f71439c68fc14d7415a876cab50095e
gurpreetsingh <gurpreet24277@gmail.com>
2023-05-08 20:40:26 -0400

    H2 DB Changes

--- src/main/java/com/CourseManagement/Management/todo/TodoControllerJPA.java
+++ src/main/java/com/CourseManagement/Management/todo/TodoControllerJPA.java
@@ -14,19 +14,19 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import java.time.LocalDate;
 import java.util.List;
 
-//@Controller
+@Controller
 @SessionAttributes("name")
-public class TodoController {
-    private TodoService todoService;
+public class TodoControllerJPA {
+    private TodoRepository todoRepository;
 
-    public TodoController(TodoService todoService) {
-        this.todoService = todoService;
+    public TodoControllerJPA(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
     }
 
     @RequestMapping("/list-todos")
     public String listAllTodos(ModelMap model) {
         String username = getLoggedInUser();
-        List<Todo> data = todoService.findByUsername(username);
+        List<Todo> data = todoRepository.findByUsername(username);
         model.put("todos", data);
         model.addAttribute("name");
         return "listTodos";
@@ -46,21 +46,23 @@ public class TodoController {
         if (result.hasErrors()) {
             return "todo";
         }
+
         String username = getLoggedInUser();
-        todoService.addTodo(todo.getDescription(), username, todo.getTargetDate());
+        todo.setUsername(username);
+        todoRepository.save(todo);
         return "redirect:list-todos";
     }
 
     @RequestMapping(value = "/delete-todo", method = RequestMethod.GET)
     public String deleteTodo(@RequestParam int id) {
-        todoService.deleteTodo(id);
+        todoRepository.deleteById(id);
         return "redirect:list-todos";
     }
 
     @RequestMapping(value = "/update-todo", method = RequestMethod.GET)
     public String updateTodoGET (@RequestParam int id, ModelMap model) {
         model.put("request", "delete");
-        Todo todo = todoService.findTodoById(id);
+        Todo todo = todoRepository.findById(id).get();
         model.addAttribute("todo", todo);
         return "todo";
     }
@@ -74,7 +76,7 @@ public class TodoController {
 
         String username = getLoggedInUser();
         todo.setUsername(username);
-        todoService.updateTodo(todo);
+        todoRepository.save(todo);
         return "redirect:list-todos";
     }
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (1001, 'Gurpreet Singh', 'Learn AWS Cloud', CURRENT_DATE, false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (1002, 'Gurpreet Singh', 'Get NodeJs Certification', CURRENT_DATE, false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (1003, 'Jagveer Singh', 'Get React Js Certification', CURRENT_DATE, false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (1004, 'Jagveer Singh', 'Learn AWS and get Certified', CURRENT_DATE, false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (1005, 'Gurpreet Singh', 'Get Spring Boot Certification', CURRENT_DATE, false);
